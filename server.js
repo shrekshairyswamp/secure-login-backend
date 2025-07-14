6352119b809249f05a4f5eb581bd02e20fa1c632
actual droid <97837120+shrekshairyswamp@users.noreply.github.com>
2025-07-14 22:04:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -49,29 +49,5 @@ app.post("/api/login", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(✅ Server is running on port ${PORT});
+  console.log(`✅ Server is running on port ${PORT}`);
 });
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://admin:<db_password>@beststudyguide.q4krgkm.mongodb.net/?retryWrites=true&w=majority&appName=BestStudyGuide";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
